fcaf9e63d1c56dd9814c89ad4450efacfe1bdfd1
EBot <66640213+ebotdabest@users.noreply.github.com>
2023-09-24 17:09:33 +0200

    Update makevideo.py

--- makevideo.py
+++ makevideo.py
@@ -9,9 +9,7 @@ def make_post_video(id):
 
     base_vid = editor.VideoFileClip("subway.mp4")
 
-    txt_clip = (editor.TextClip("Super idol", fontsize=70, color='white')
-                .set_position('center')
-                .set_duration(10))
+    
 
     end_video = editor.CompositeVideoClip([base_vid, txt_clip])
     end_video.write_videofile(os.path.join(EXPORT_FOLDER, id, "video.mp4"))
